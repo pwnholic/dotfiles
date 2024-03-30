74017fb1b9bc1932df3effbd88ffb1d2e56a33c7
wigtono <wigtono41@gmail.com>
2024-03-30 19:39:21 +0700

    i just to fucking lazy for write some commit message

--- nvim/init.lua
+++ nvim/init.lua
@@ -31,16 +31,54 @@ end
 
 require("lazy").setup({
 	{ import = "plugins" },
-	{ name = "autocmds", main = "autocmds", dir = config_path, event = "VeryLazy", config = true },
-	{ name = "keymaps", main = "keymaps", dir = config_path, event = "VeryLazy", config = true },
-	{ name = "tmux", main = "tmux", dir = config_path, event = "BufRead", config = true },
-	{ name = "buffer", main = "buffer", dir = config_path, event = "BufRead", config = true },
+	{
+		name = "autocmds",
+		main = "autocmds",
+		dir = config_path,
+		event = "VeryLazy",
+		config = true,
+	},
+	{
+		name = "keymaps",
+		main = "keymaps",
+		dir = config_path,
+		event = "VeryLazy",
+		config = true,
+	},
+	{
+		name = "tmux",
+		main = "tmux",
+		dir = config_path,
+		event = "BufRead",
+		config = true,
+	},
+	{
+		name = "buffer",
+		main = "buffer",
+		dir = config_path,
+		event = "BufRead",
+		config = true,
+	},
 }, {
-	defaults = { lazy = true, version = "*" },
-	install = { missing = true, colorscheme = { "tokyonight" } },
+	defaults = {
+		lazy = true,
+		version = "*",
+	},
+	install = {
+		missing = true,
+		colorscheme = { "tokyonight" },
+	},
+	change_detection = {
+		enabled = true,
+		notify = false,
+	},
+	checker = {
+		enabled = true,
+		notify = false,
+		frequency = (3600 * 24) * 7,
+	},
 	diff = { cmd = "diffview.nvim" },
 	ui = { border = "single" },
-	change_detection = { enabled = true, notify = false },
 	performance = {
 		cache = { enabled = true },
 		reset_packpath = true,
