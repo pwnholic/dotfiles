d80a71dedba84396a2748e662cc8d6aed3d25c9b
pwnholic <wigtono41@gmail.com>
2024-08-06 17:13:15 +0700

    move to this shitt

--- nvim/lua/utils/tmux.lua
+++ nvim/lua/utils/tmux.lua
@@ -1,3 +1,5 @@
+local M = {}
+
 ---@alias nvim_direction_t 'h'|'j'|'k'|'l'
 ---@alias tmux_direction_t 'L'|'D'|'U'|'R'
 ---@alias tmux_borderpane_direction_t 'left'|'bottom'|'top'|'right'
@@ -21,7 +23,7 @@ end
 ---Get tmux option value in current pane
 ---@param opt string tmux pane option
 ---@return string tmux pane option value
-local function tmux_get_pane_opt(opt)
+function M.tmux_get_pane_opt(opt)
 	return (
 		tmux_exec(string.format("display-message -pt %s '#{%s}'", vim.env.TMUX_PANE, vim.fn.escape(opt, "'\\"))):gsub(
 			"\n.*",
@@ -34,20 +36,20 @@ end
 ---@param opt string tmux pane option
 ---@param val string tmux pane option value
 ---@return nil
-local function tmux_set_pane_opt(opt, val)
+function M.tmux_set_pane_opt(opt, val)
 	tmux_exec(string.format("set -pt %s %s '%s'", vim.env.TMUX_PANE, opt, vim.fn.escape(val, "'\\")))
 end
 
 ---Unset a tmux pane option
 ---@param opt string tmux pane option
 ---@return nil
-local function tmux_unset_pane_opt(opt)
+function M.tmux_unset_pane_opt(opt)
 	tmux_exec(string.format("set -put %s '%s'", vim.env.TMUX_PANE, vim.fn.escape(opt, "'\\")))
 end
 
 ---@return boolean
 local function tmux_is_zoomed()
-	return tmux_get_pane_opt("window_zoomed_flag") == "1"
+	return M.tmux_get_pane_opt("window_zoomed_flag") == "1"
 end
 
 ---@type table<nvim_direction_t, tmux_borderpane_direction_t>
@@ -61,7 +63,7 @@ local tmux_pane_position_map = {
 ---@param direction nvim_direction_t
 ---@return boolean
 local function tmux_at_border(direction)
-	return tmux_get_pane_opt("pane_at_" .. tmux_pane_position_map[direction]) == "1"
+	return M.tmux_get_pane_opt("pane_at_" .. tmux_pane_position_map[direction]) == "1"
 end
 
 ---@param direction nvim_direction_t
@@ -139,7 +141,7 @@ end
 
 ---@param direction nvim_direction_t
 ---@return function: rhs of a window navigation keymap
-local function navigate_wrap(direction)
+function M.navigate_wrap(direction)
 	return function()
 		navigate(direction, vim.v.count1)
 	end
@@ -151,22 +153,22 @@ local function tmux_mapkey_default_condition()
 end
 
 ---@return boolean
-local function tmux_mapkey_close_win_condition()
+function M.tmux_mapkey_close_win_condition()
 	return not tmux_is_zoomed() and nvim_has_only_win()
 end
 
 ---@return boolean
-local function tmux_mapkey_resize_pane_horiz_condition()
+function M.tmux_mapkey_resize_pane_horiz_condition()
 	return not tmux_is_zoomed() and (nvim_at_border("l") and (nvim_at_border("h") or not tmux_at_border("l")))
 end
 
 ---@return boolean
-local function tmux_mapkey_resize_pane_vert_condition()
+function M.tmux_mapkey_resize_pane_vert_condition()
 	return not tmux_is_zoomed() and (nvim_at_border("j") and (nvim_at_border("k") or not tmux_at_border("j")))
 end
 
 ---@return fun(): boolean
-local function tmux_mapkey_navigate_condition(direction)
+function M.tmux_mapkey_navigate_condition(direction)
 	return function()
 		return (nvim_at_border(direction) or nvim_in_floating_win()) and tmux_should_move(direction)
 	end
@@ -177,7 +179,7 @@ end
 ---@param action string|function
 ---@param condition? fun(): boolean
 ---@return nil
-local function tmux_mapkey_fallback(key, action, condition)
+function M.tmux_mapkey_fallback(key, action, condition)
 	condition = condition or tmux_mapkey_default_condition
 	require("utils.keys").amend({ "n", "x", "t" }, key, function(fallback)
 		if not condition() or vim.env.NVIM or vim.fn.mode():sub(1, 1) == "t" and require("utils").running_tui() then
@@ -192,59 +194,4 @@ local function tmux_mapkey_fallback(key, action, condition)
 	end)
 end
 
----Setup keymaps and tmux variable `@is_vim`
----@return nil
-return function()
-	if vim.g.loaded_tmux or not vim.env.TMUX or not vim.g.has_ui then
-		return
-	end
-	vim.g.loaded_tmux = true
-
-	tmux_mapkey_fallback("<M-h>", navigate_wrap("h"), tmux_mapkey_navigate_condition("h"))
-	tmux_mapkey_fallback("<M-j>", navigate_wrap("j"), tmux_mapkey_navigate_condition("j"))
-	tmux_mapkey_fallback("<M-k>", navigate_wrap("k"), tmux_mapkey_navigate_condition("k"))
-	tmux_mapkey_fallback("<M-l>", navigate_wrap("l"), tmux_mapkey_navigate_condition("l"))
-
-	tmux_mapkey_fallback("<M-p>", "last-pane")
-	tmux_mapkey_fallback("<M-R>", "swap-pane -U")
-	tmux_mapkey_fallback("<M-r>", "swap-pane -D")
-	tmux_mapkey_fallback("<M-o>", "confirm 'kill-pane -a'")
-	tmux_mapkey_fallback("<M-=>", "confirm 'select-layout tiled'")
-	tmux_mapkey_fallback("<M-c>", "confirm kill-pane", tmux_mapkey_close_win_condition)
-	tmux_mapkey_fallback("<M-q>", "confirm kill-pane", tmux_mapkey_close_win_condition)
-	tmux_mapkey_fallback("<M-<>", "resize-pane -L 4", tmux_mapkey_resize_pane_horiz_condition)
-	tmux_mapkey_fallback("<M->>", "resize-pane -R 4", tmux_mapkey_resize_pane_horiz_condition)
-	tmux_mapkey_fallback("<M-,>", "resize-pane -L 4", tmux_mapkey_resize_pane_horiz_condition)
-	tmux_mapkey_fallback("<M-.>", "resize-pane -R 4", tmux_mapkey_resize_pane_horiz_condition)
-	tmux_mapkey_fallback(
-		"<M-->",
-		[[run "tmux resize-pane -y $(($(tmux display -p '#{pane_height}') - 2))"]],
-		tmux_mapkey_resize_pane_vert_condition
-	)
-	tmux_mapkey_fallback(
-		"<M-+>",
-		[[run "tmux resize-pane -y $(($(tmux display -p '#{pane_height}') + 2))"]],
-		tmux_mapkey_resize_pane_vert_condition
-	)
-
-	-- Set @is_vim and register relevant autocmds callbacks if not already
-	-- in a vim/nvim session
-	if tmux_get_pane_opt("@is_vim") == "" then
-		tmux_set_pane_opt("@is_vim", "yes")
-		local groupid = vim.api.nvim_create_augroup("TmuxNavSetIsVim", {})
-		vim.api.nvim_create_autocmd("VimResume", {
-			desc = "Set @is_vim in tmux pane options after vim resumes.",
-			group = groupid,
-			callback = function()
-				tmux_set_pane_opt("@is_vim", "yes")
-			end,
-		})
-		vim.api.nvim_create_autocmd({ "VimSuspend", "VimLeave" }, {
-			desc = "Unset @is_vim in tmux pane options on vim leaving or suspending.",
-			group = groupid,
-			callback = function()
-				tmux_unset_pane_opt("@is_vim")
-			end,
-		})
-	end
-end
+return M
--- nvim/plugin/_load.lua
+++ nvim/plugin/_load.lua
@@ -94,5 +94,57 @@ vim.api.nvim_create_autocmd({ "LspAttach", "DiagnosticChanged" }, {
 })
 
 vim.schedule(function()
-	require("utils.tmux")()
+	local utils = require("utils.tmux")
+	if vim.g.loaded_tmux or not vim.env.TMUX then
+		return
+	end
+	vim.g.loaded_tmux = true
+
+	utils.tmux_mapkey_fallback("<M-h>", utils.navigate_wrap("h"), utils.tmux_mapkey_navigate_condition("h"))
+	utils.tmux_mapkey_fallback("<M-j>", utils.navigate_wrap("j"), utils.tmux_mapkey_navigate_condition("j"))
+	utils.tmux_mapkey_fallback("<M-k>", utils.navigate_wrap("k"), utils.tmux_mapkey_navigate_condition("k"))
+	utils.tmux_mapkey_fallback("<M-l>", utils.navigate_wrap("l"), utils.tmux_mapkey_navigate_condition("l"))
+
+	utils.tmux_mapkey_fallback("<M-p>", "last-pane")
+	utils.tmux_mapkey_fallback("<M-R>", "swap-pane -U")
+	utils.tmux_mapkey_fallback("<M-r>", "swap-pane -D")
+	utils.tmux_mapkey_fallback("<M-o>", "confirm 'kill-pane -a'")
+	utils.tmux_mapkey_fallback("<M-=>", "confirm 'select-layout tiled'")
+	utils.tmux_mapkey_fallback("<M-c>", "confirm kill-pane", utils.tmux_mapkey_close_win_condition)
+	utils.tmux_mapkey_fallback("<M-q>", "confirm kill-pane", utils.tmux_mapkey_close_win_condition)
+	utils.tmux_mapkey_fallback("<M-<>", "resize-pane -L 4", utils.tmux_mapkey_resize_pane_horiz_condition)
+	utils.tmux_mapkey_fallback("<M->>", "resize-pane -R 4", utils.tmux_mapkey_resize_pane_horiz_condition)
+	utils.tmux_mapkey_fallback("<M-,>", "resize-pane -L 4", utils.tmux_mapkey_resize_pane_horiz_condition)
+	utils.tmux_mapkey_fallback("<M-.>", "resize-pane -R 4", utils.tmux_mapkey_resize_pane_horiz_condition)
+	utils.tmux_mapkey_fallback(
+		"<M-->",
+		[[run "tmux resize-pane -y $(($(tmux display -p '#{pane_height}') - 2))"]],
+		utils.tmux_mapkey_resize_pane_vert_condition
+	)
+	utils.tmux_mapkey_fallback(
+		"<M-+>",
+		[[run "tmux resize-pane -y $(($(tmux display -p '#{pane_height}') + 2))"]],
+		utils.tmux_mapkey_resize_pane_vert_condition
+	)
+
+	-- Set @is_vim and register relevant autocmds callbacks if not already
+	-- in a vim/nvim session
+	if utils.tmux_get_pane_opt("@is_vim") == "" then
+		utils.tmux_set_pane_opt("@is_vim", "yes")
+		local groupid = vim.api.nvim_create_augroup("TmuxNavSetIsVim", {})
+		vim.api.nvim_create_autocmd("VimResume", {
+			desc = "Set @is_vim in tmux pane options after vim resumes.",
+			group = groupid,
+			callback = function()
+				utils.tmux_set_pane_opt("@is_vim", "yes")
+			end,
+		})
+		vim.api.nvim_create_autocmd({ "VimSuspend", "VimLeave" }, {
+			desc = "Unset @is_vim in tmux pane options on vim leaving or suspending.",
+			group = groupid,
+			callback = function()
+				utils.tmux_unset_pane_opt("@is_vim")
+			end,
+		})
+	end
 end)
