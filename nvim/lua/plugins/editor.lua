d21bec798ac292e4acccdc607b127e2576d98f59
pwnholic <wigtono41@gmail.com>
2024-08-15 15:29:24 +0700

    markdown hokya hokya

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1192,6 +1192,23 @@ return {
 		"echasnovski/mini-git",
 		main = "mini.git",
 		cmd = "Git",
+		keys = function()
+			return {
+				{
+					"<leader>hg",
+					function()
+						vim.ui.input({ prompt = "Git Options : ", completion = "command" }, function(args)
+							if args == "" then
+								return vim.notify("Git need argument", 4, { title = "Git" })
+							else
+								vim.cmd.Git(args)
+							end
+						end)
+					end,
+					desc = "Git Wrapper",
+				},
+			}
+		end,
 		opts = function()
 			return {
 				job = { git_executable = "git", timeout = 30000 },
