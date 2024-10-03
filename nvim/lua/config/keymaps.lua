8e10799070ad4bea63fb9647b99210da3ece2922
pwnholic <wigtono41@gmail.com>
2024-10-03 19:46:59 +0700

    fix: change `ShouldBindQuery` to `ShouldBindJSON`

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -19,27 +19,7 @@ vim.keymap.set("i", "<A-n>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
 vim.keymap.set("v", "<A-p>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
 vim.keymap.set("v", "<A-n>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
 
-if not vim.env.TMUX then
-    return
-else
-    local utils = require("utils")
-    -- stylua: ignore start
-    utils.tmux.tmux_mapkey_fallback("<M-h>", utils.tmux.navigate_wrap("h"), utils.tmux.tmux_mapkey_navigate_condition("h"))
-    utils.tmux.tmux_mapkey_fallback("<M-j>", utils.tmux.navigate_wrap("j"), utils.tmux.tmux_mapkey_navigate_condition("j"))
-    utils.tmux.tmux_mapkey_fallback("<M-k>", utils.tmux.navigate_wrap("k"), utils.tmux.tmux_mapkey_navigate_condition("k"))
-    utils.tmux.tmux_mapkey_fallback("<M-l>", utils.tmux.navigate_wrap("l"), utils.tmux.tmux_mapkey_navigate_condition("l"))
-
-    utils.tmux.tmux_mapkey_fallback("<M-R>", "swap-pane -U")
-    utils.tmux.tmux_mapkey_fallback("<M-r>", "swap-pane -D")
-    utils.tmux.tmux_mapkey_fallback("<M-o>", "confirm 'kill-pane -a'")
-    utils.tmux.tmux_mapkey_fallback("<M-=>", "confirm 'select-layout tiled'")
-    utils.tmux.tmux_mapkey_fallback("<M-c>", "confirm kill-pane", utils.tmux.tmux_mapkey_close_win_condition)
-    utils.tmux.tmux_mapkey_fallback("<M-q>", "confirm kill-pane", utils.tmux.tmux_mapkey_close_win_condition)
-    utils.tmux.tmux_mapkey_fallback("<M-<>", "resize-pane -L 4", utils.tmux.tmux_mapkey_resize_pane_horiz_condition)
-    utils.tmux.tmux_mapkey_fallback("<M->>", "resize-pane -R 4", utils.tmux.tmux_mapkey_resize_pane_horiz_condition)
-    utils.tmux.tmux_mapkey_fallback("<M-,>", "resize-pane -L 4", utils.tmux.tmux_mapkey_resize_pane_horiz_condition)
-    utils.tmux.tmux_mapkey_fallback("<M-.>", "resize-pane -R 4", utils.tmux.tmux_mapkey_resize_pane_horiz_condition)
-    utils.tmux.tmux_mapkey_fallback( "<M-->", [[run "tmux resize-pane -y $(($(tmux display -p '#{pane_height}') - 2))"]], utils.tmux.tmux_mapkey_resize_pane_vert_condition)
-    utils.tmux.tmux_mapkey_fallback( "<M-+>", [[run "tmux resize-pane -y $(($(tmux display -p '#{pane_height}') + 2))"]], utils.tmux.tmux_mapkey_resize_pane_vert_condition)
-    -- stylua: ignore end
-end
+vim.keymap.set({ "n", "x", "t", "i" }, "<M-h>", require("utils.tmux").navigate_wrap("h"))
+vim.keymap.set({ "n", "x", "t", "i" }, "<M-j>", require("utils.tmux").navigate_wrap("j"))
+vim.keymap.set({ "n", "x", "t", "i" }, "<M-k>", require("utils.tmux").navigate_wrap("k"))
+vim.keymap.set({ "n", "x", "t", "i" }, "<M-l>", require("utils.tmux").navigate_wrap("l"))
