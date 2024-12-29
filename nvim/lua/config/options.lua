c58f216e029b9212c3fde05d456bb9f720b48ebf
pwnholic <wigtono41@gmail.com>
2024-12-29 13:14:23 +0700

    change router for outlet group download

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -45,5 +45,14 @@ vim.opt.backupdir:remove(".")
 vim.g.lazyvim_blink_main = true -- set to true
 vim.g.lazyvim_picker = "fzf"
 vim.g.border = "single"
+
+-- LSP Server to use for Python.
+-- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = "basedpyright"
+-- Set to "ruff_lsp" to use the old LSP implementation version.
 vim.g.lazyvim_python_ruff = "ruff"
+
+-- Set to "bacon-ls" to use bacon-ls instead of rust-analyzer.
+-- only for diagnostics. The rest of LSP support will still be
+-- provided by rust-analyzer.
+vim.g.lazyvim_rust_diagnostics = "bacon-ls"
--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -12,5 +12,25 @@ return {
             end,
             desc = "Harpoon Quick Menu",
         },
+        {
+            "<A-space>",
+            function()
+                local harpoon = require("harpoon")
+                harpoon.ui:toggle_quick_menu(
+                    harpoon:list(),
+                    { ui_width_ratio = 0.45, border = vim.g.border, title = "" }
+                )
+            end,
+            desc = "Harpoon Quick Menu",
+        },
+        {
+            "<A-a>",
+            function()
+                require("harpoon"):list():add()
+                local msg = "Add this file to harpoon list"
+                vim.notify(msg, 2, { title = "Harpoon" })
+            end,
+            desc = "Harpoon File",
+        },
     },
 }
