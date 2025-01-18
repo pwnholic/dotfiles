0c0ed728425f83d231831e12bf1dd792343fca93
pwnholic <wigtono41@gmail.com>
2025-01-18 19:09:09 +0700

    fix loop trought filters duplicate result

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,5 +1,24 @@
 return {
     "ThePrimeagen/harpoon",
+    config = function()
+        local harpoon = require("harpoon")
+        harpoon:setup()
+        harpoon:extend({
+            UI_CREATE = function(cx)
+                vim.keymap.set("n", "<C-v>", function()
+                    harpoon.ui:select_menu_item({ vsplit = true })
+                end, { buffer = cx.bufnr })
+
+                vim.keymap.set("n", "<C-s>", function()
+                    harpoon.ui:select_menu_item({ split = true })
+                end, { buffer = cx.bufnr })
+
+                vim.keymap.set("n", "<C-t>", function()
+                    harpoon.ui:select_menu_item({ tabedit = true })
+                end, { buffer = cx.bufnr })
+            end,
+        })
+    end,
     keys = {
         {
             "<leader>h",
