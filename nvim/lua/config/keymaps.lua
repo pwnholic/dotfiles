aa9f93464b6720cd9a1707c6d440e811becfa4e2
pwnholic <wigtono41@gmail.com>
2025-01-28 14:31:10 +0700

    fix like to ilike

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n", "gQ", "mzgggqG`z<cmd>delmarks z<cr>zz", { desc = "Format buffer" })
 vim.keymap.set({ "i", "c" }, "<C-l>", "<C-o>A", { desc = "Go to the end of the line" })
+
+local Keys = require("lazyvim.plugins.lsp.keymaps").get()
--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,13 +1,17 @@
 return {
     "neovim/nvim-lspconfig",
-    opts = {
-        codelens = { enabled = false },
-        inlay_hints = { enabled = false, exclude = {} },
-        diagnostics = {
+    opts = function(_, opts)
+        local keys = require("lazyvim.plugins.lsp.keymaps").get()
+        keys[#keys + 1] = { "gd", "<cmd>Trouble lsp_definitions<cr>" }
+        keys[#keys + 1] = { "gr", "<cmd>Trouble lsp_references<cr>" }
+
+        opts.codelens = { enabled = false }
+        opts.inlay_hints = { enabled = false, exclude = {} }
+        opts.diagnostics = {
             float = { border = vim.g.border },
             virtual_text = { spacing = 2, source = "if_many", prefix = "" },
-        },
-        servers = {},
-        setup = {},
-    },
+        }
+        opts.servers = {}
+        opts.setup = {}
+    end,
 }
