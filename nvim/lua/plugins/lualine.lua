bcbce0df870f21babdadeea575c00aad970e02d7
pwnholic <wigtono41@gmail.com>
2025-04-14 09:42:49 +0700

    fix conflict

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -36,20 +36,10 @@ return {
             })
 
             table.insert(opts.sections.lualine_y, {
-                function()
-                    local client_names = {}
-                    local current_buf = vim.api.nvim_get_current_buf()
-                    if vim.api.nvim_buf_is_valid(0) then
-                        for _, server in pairs(vim.lsp.get_clients({ bufnr = current_buf })) do
-                            table.insert(client_names, server.name)
-                        end
-                        return string.format("%s", table.concat(client_names, " "))
-                    end
-                end,
-                cond = function()
-                    return next(vim.lsp.get_clients({ bufnr = 0 })) ~= nil
-                end,
-                -- color = "LualineLspClient",
+                "lsp_status",
+                icon = false,
+                symbols = { separator = " ", done = "" },
+                ignore_lsp = { "null-ls" },
             })
 
             table.insert(opts.sections.lualine_x, {
