6c2e6f24e605a9b169bf6ecc39d0fc9f9ae81ffa
pwnholic <wigtono41@gmail.com>
2025-04-18 16:18:01 +0700

    add validation

---
+++ nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,11 @@
+return {
+    "m4xshen/hardtime.nvim",
+    cmd = "Hardtime",
+    event = "VeryLazy",
+    version = false,
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {
+        disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil", "fzf" },
+        max_count = 4,
+    },
+}
