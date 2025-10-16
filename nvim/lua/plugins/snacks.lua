50acb5f0dbe69a014a888f6ce2a0a4f499424bd0
pwnholic <wigtono41@gmail.com>
2025-10-16 16:03:10 +0700

    add new nvim

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,5 +1,25 @@
 local indent_char = "▏"
 
+local ivy_split = {
+    preview = "main",
+    layout = {
+        box = "vertical",
+        backdrop = false,
+        width = 0,
+        height = 0.4,
+        position = "bottom",
+        border = "none",
+        title = " {title} {live} {flags}",
+        title_pos = "left",
+        { win = "input", height = 1, border = "bottom" },
+        {
+            box = "horizontal",
+            { win = "list", border = "none" },
+            { win = "preview", title = "{preview}", width = 0.6, border = "left" },
+        },
+    },
+}
+
 return {
     {
         "folke/snacks.nvim",
@@ -72,7 +92,7 @@ return {
                             title_pos = "center",
                             { win = "input", height = 1, border = "bottom" },
                             { win = "list", border = "none" },
-                            { win = "preview", title = "{preview}", height = 0.4, border = "top" },
+                            { win = "preview", title = "{preview}", height = 0.4, border = vim.o.winborder },
                         },
                     },
                     sidebar = {
@@ -96,6 +116,18 @@ return {
                             { win = "preview", title = "{preview}", height = 0.4, border = "top" },
                         },
                     },
+                },
+                sources = {
+                    files = { layout = ivy_split },
+                    lsp_declarations = { layout = ivy_split },
+                    lsp_definitions = { layout = ivy_split },
+                    lsp_implementations = { layout = ivy_split },
+                    lsp_references = { layout = ivy_split },
+                    lsp_symbols = { layout = ivy_split },
+                    lsp_type_definitions = { layout = ivy_split },
+                    lsp_workspace_symbols = { layout = ivy_split },
+                    diagnostics = { layout = ivy_split },
+                    diagnostics_buffer = { layout = ivy_split },
                 },
             },
         },
