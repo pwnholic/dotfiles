654d7c6e2182cfc8916c5262954eee6dd8143d3d
pwnholic <wigtono41@gmail.com>
2025-10-17 14:35:23 +0700

    fix logging

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,12 +6,12 @@ vim.api.nvim_create_autocmd("ModeChanged", {
     desc = "Enable inlay hints only in normal mode",
     callback = function(args)
         local bufnr = args.buf
-        if not vim.api.nvim_buf_is_valid(bufnr) then
-            return
-        end
-
         local to_mode = args.match:sub(3, 3)
         local clients = vim.lsp.get_clients({ bufnr = bufnr, method = "textDocument/inlayHint" })
+
+        if not vim.api.nvim_buf_is_valid(bufnr) or #clients < 1 then
+            return
+        end
 
         if next(clients) then
             vim.lsp.inlay_hint.enable(to_mode == "n", { bufnr = bufnr })
