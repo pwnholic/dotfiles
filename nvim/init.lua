098c8efc9b0fa3e01b41c346610eed1eae0215ea
Tono <wigtono41@gmail.com>
2026-08-01 14:27:18 +0700

    fix shit

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,32 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+vim.loader.enable(true)
+
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.cmdheight = 0
+opt.clipboard = "unnamedplus"
+
+
+vim.pack.add({ 'https://github.com/saghen/blink.lib', 'https://github.com/saghen/blink.cmp' })
+local cmp = require('blink.cmp')
+cmp.build():pwait()
+cmp.setup()
+
+
+vim.lsp.config['lua_ls'] = {
+	cmd = { 'lua-language-server' },
+	filetypes = { 'lua' },
+	root_markers = {},
+	settings = {
+		Lua = {
+			runtime = {
+				version = 'LuaJIT',
+			}
+		}
+	}
+}
+
+vim.lsp.enable('lua_ls')
+
 
