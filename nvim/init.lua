53fecc6648b8d5598f70613ea3dc1b46e0f66944
Tono <wigtono41@gmail.com>
2026-08-02 21:42:29 +0700

    fix shit

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,9 @@
 vim.loader.enable(true)
 
 local opt = vim.opt
-
 opt.number = true
 opt.relativenumber = true
-opt.cmdheight = 0
 opt.clipboard = "unnamedplus"
-
 
 vim.pack.add({ 'https://github.com/saghen/blink.lib', 'https://github.com/saghen/blink.cmp' })
 local cmp = require('blink.cmp')
@@ -14,7 +11,7 @@ cmp.build():pwait()
 cmp.setup()
 
 
-vim.lsp.config['lua_ls'] = {
+vim.lsp.config.lua_ls = {
 	cmd = { 'lua-language-server' },
 	filetypes = { 'lua' },
 	root_markers = {},
@@ -27,6 +24,13 @@ vim.lsp.config['lua_ls'] = {
 	}
 }
 
+
 vim.lsp.enable('lua_ls')
 
+vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = "*.lua",
+	callback = function()
+		vim.lsp.buf.format()
+	end
 
+})
